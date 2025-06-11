a7a89359b4dc25a5a06475eaf73e28defada6fcf
Aaron Meza <aaron.meza@gmail.com>
2025-06-10 23:41:02 -0500

    update email address

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,7 +2,7 @@ export default function Contact() {
   return (
     <div className="prose mx-auto">
       <h1>Contact Us</h1>
-      <p>Email: <a href="mailto:hello@redwoods.wine">hello@redwoods.wine</a></p>
+      <p>Email: <a href="mailto:aaron@redwoods.wine">aaron@redwoods.wine</a></p>
       <p>Phone: (612) 293‑6392</p>
       <p>Address: 512 Division St, Northfield, MN</p>
       <iframe
